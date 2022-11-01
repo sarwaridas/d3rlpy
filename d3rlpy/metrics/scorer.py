a49382ec14b2fd336ec5d91a5e39bb44dfc6d3ef
Sarwari Das <sarwaridas2@gmail.com>
2022-11-01 11:05:45 -0400

    updated metric for true Q

--- d3rlpy/metrics/scorer.py
+++ d3rlpy/metrics/scorer.py
@@ -586,7 +586,7 @@ def true_q_evaluate(algo: AlgoProtocol, episodes: List[Episode]) -> float:
                 [batch.next_observations[0]], next_actions
             )
             mask = (1.0 - np.asarray(batch.terminals)).reshape(-1)
-            rewards = np.asarray(batch.next_rewards).reshape(-1)
+            rewards = np.asarray(batch.rewards).reshape(-1)
             if algo.reward_scaler:
                 rewards = algo.reward_scaler.transform_numpy(rewards)
             y = rewards + algo.gamma * cast(np.ndarray, next_values) * mask
